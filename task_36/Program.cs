711f12d1b6e17993a360d7c474fd67c1b42494a0
Daria <macdaria1999@gmail.com>
2022-12-11 17:31:44 +0300

    Add task 36

---
+++ task_36/Program.cs
@@ -0,0 +1,23 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-20, 101); // [-20, 100]
+}
+int ReleaseArray(int[] array)
+{
+    int sum1 = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 1)
+            sum1 += array[i];
+    }
+    return sum1;
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
+Console.WriteLine($"Сумма четных чисел: {ReleaseArray(array)}");
