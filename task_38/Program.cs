cc1a6e61ff493bb9a583a4b35b341ade5d4f3663
Daria <macdaria1999@gmail.com>
2022-12-12 15:36:37 +0300

    Add task 38

---
+++ task_38/Program.cs
@@ -0,0 +1,34 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-20, 101); // [-20, 100]
+}
+void ReleaseArray(int[] array)
+{
+int min = array [0];
+int max = array [0];
+int minInd = 0;
+int maxInd = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min)
+    {
+        min = array[i];
+        minInd = i;
+    }
+    else if (array[i] > max)
+    {
+        max = array[i];
+        maxInd = i;
+    }
+}
+for (int i = minInd; i < maxInd; i++)
+Console.Write (array [i]);
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
